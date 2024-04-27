a86997fdb826612e822564f45dde6fb973b94724
Rushi <rgurav2311@gmail.com>
2024-04-27 10:05:06 +0530

    New Commit

--- src/Array.js
+++ src/Array.js
@@ -2,31 +2,32 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 
 function Array() {
+  const [arr, SetArr] = useState("");
 
-    const [arr, SetArr] = useState('');
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(arr);
+    let array = Array.form(...arr);
+    console.log(array);
+  }
 
-    function handleSubmit(e){
-        e.preventDefault();
-            console.log(arr);
-            let array = Array.form(...arr);
-            console.log(array);
-    }
-
-    for(var i = 0; i<=5; i++){
-      useEffect(()=>{
-        console.log('hii');
-      })
-    }
-
+  for (var i = 0; i <= 5; i++) {
+    useEffect(() => {
+      console.log("hii");
+    });
+  }
 
   return (
     <div className="container">
       <h4 className="mx-auto pu-4">this is the array sort method!</h4>
-
       <div className="">
         <form action="#" onClick={handleSubmit}>
-            <input type="text" onChange={(e)=> SetArr(e.target.value)} required/>
-            <button type="submit">Submit</button>
+          <input
+            type="text"
+            onChange={(e) => SetArr(e.target.value)}
+            required
+          />
+          <button type="submit">Submit</button>
         </form>
       </div>
     </div>
